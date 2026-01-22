4f516f659ab61e1d00ccada813db52f8725b4f9d
Erik Rasmussen <erik.rasmussen@unmango.dev>
2026-01-22 13:30:06 -0600

    Default osfs

--- osfs/fs.go
+++ osfs/fs.go
@@ -5,8 +5,10 @@ import (
 	"github.com/unstoppablemango/ihfs"
 )
 
+var Default ihfs.Os = Fs{os.System}
+
 type Fs struct{ os.Fs }
 
 func New() ihfs.Os {
-	return &Fs{os.System}
+	return Fs{os.System}
 }
