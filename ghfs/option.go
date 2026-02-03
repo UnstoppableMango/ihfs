0c5112a6f2e5f78fb21d41cfe009ec2d91174da8
Erik Rasmussen <erik.rasmussen@unmango.dev>
2026-02-03 12:35:40 -0600

    WIP

---
+++ ghfs/option.go
@@ -0,0 +1,25 @@
+package ghfs
+
+import (
+	"net/http"
+
+	"github.com/google/go-github/v82/github"
+)
+
+type Option func(*Fs)
+
+func WithClient(client *github.Client) Option {
+	return func(f *Fs) {
+		f.client = client
+	}
+}
+
+func WithHttpClient(client *http.Client) Option {
+	return WithClient(github.NewClient(client))
+}
+
+func WithContextFunc(fn ContextFunc) Option {
+	return func(f *Fs) {
+		f.ctxFn = fn
+	}
+}
