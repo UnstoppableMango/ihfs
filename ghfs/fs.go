e7deaa296eef9f82320d985d7fe13e91d98e0754
Erik Rasmussen <erik.rasmussen@unmango.dev>
2026-02-03 11:33:43 -0600

    Little bit of make
    WIP
    
    WIP
    
    WIP
    
    WIP
    
    WIP
    
    WIP
    
    WIP
    
    WIP
    
    WIP

--- ghfs/fs.go
+++ ghfs/fs.go
@@ -1,12 +1,31 @@
 package ghfs
 
 import (
+	"context"
+	"strings"
+
 	"github.com/google/go-github/v82/github"
+	"github.com/unmango/go/fopt"
 	"github.com/unstoppablemango/ihfs"
+)
+
+type (
+	ContextFunc func(*Fs, ihfs.Operation) context.Context
 )
 
 type Fs struct {
 	client *github.Client
+	ctxFn  ContextFunc
+}
+
+func New(options ...Option) *Fs {
+	f := &Fs{
+		client: github.NewClient(nil),
+		ctxFn:  background,
+	}
+
+	fopt.ApplyAll(f, options)
+	return f
 }
 
 func (*Fs) Name() string {
@@ -14,5 +33,59 @@ func (*Fs) Name() string {
 }
 
 func (f *Fs) Open(name string) (ihfs.File, error) {
-	return nil, nil
+	parts := strings.Split(clean(name), "/")
+
+	switch len(parts) {
+	case 1:
+		return &Owner{
+			name: parts[0],
+		}, nil
+	case 2:
+		return &Repository{
+			owner: parts[0],
+			name:  parts[1],
+		}, nil
+	case 4:
+		return &Branch{
+			owner:      parts[0],
+			repository: parts[1],
+			name:       parts[3],
+		}, nil
+	case 5:
+		if parts[2] == "blob" {
+			return &Content{
+				owner:      parts[0],
+				repository: parts[1],
+				branch:     parts[3],
+				name:       parts[4],
+			}, nil
+		}
+
+		return &Release{
+			owner:      parts[0],
+			repository: parts[1],
+			name:       parts[4],
+		}, nil
+	case 6:
+		return &Asset{
+			owner:      parts[0],
+			repository: parts[1],
+			release:    parts[4],
+			name:       parts[5],
+		}, nil
+	}
+
+	return nil, ihfs.ErrNotExist
+}
+
+func (f *Fs) setAuthToken(token string) {
+	f.client = f.client.WithAuthToken(token)
+}
+
+func (f *Fs) context(op ihfs.Operation) context.Context {
+	return f.ctxFn(f, op)
+}
+
+func background(*Fs, ihfs.Operation) context.Context {
+	return context.Background()
 }
---
+++ ghfs/fs_test.go
@@ -0,0 +1,171 @@
+package ghfs_test
+
+import (
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+
+	"github.com/unstoppablemango/ihfs/ghfs"
+)
+
+var _ = Describe("Fs", func() {
+	Describe("Open", func() {
+		DescribeTable("should parse an owner path",
+			func(path string) {
+				fsys := ghfs.New()
+
+				f, err := fsys.Open(path)
+
+				Expect(err).NotTo(HaveOccurred())
+				Expect(f).To(BeAssignableToTypeOf(&ghfs.Owner{}))
+				o := f.(*ghfs.Owner)
+				Expect(o.Name()).To(Equal("UnstoppableMango"))
+			},
+			Entry(nil, "https://api.github.com/UnstoppableMango"),
+			Entry(nil, "https://github.com/UnstoppableMango"),
+			Entry(nil, "github.com/UnstoppableMango"),
+			Entry(nil, "api.github.com/UnstoppableMango"),
+			Entry(nil, "raw.githubusercontent.com/UnstoppableMango"),
+			Entry(nil, "UnstoppableMango"),
+		)
+
+		DescribeTable("should parse a repository path",
+			func(path string) {
+				fsys := ghfs.New()
+
+				f, err := fsys.Open(path)
+
+				Expect(err).NotTo(HaveOccurred())
+				Expect(f).To(BeAssignableToTypeOf(&ghfs.Repository{}))
+				o := f.(*ghfs.Repository)
+				Expect(o.Owner()).To(Equal("UnstoppableMango"))
+				Expect(o.Name()).To(Equal("ihfs"))
+			},
+			Entry(nil, "https://api.github.com/UnstoppableMango/ihfs"),
+			Entry(nil, "https://github.com/UnstoppableMango/ihfs"),
+			Entry(nil, "github.com/UnstoppableMango/ihfs"),
+			Entry(nil, "api.github.com/UnstoppableMango/ihfs"),
+			Entry(nil, "raw.githubusercontent.com/UnstoppableMango/ihfs"),
+			Entry(nil, "UnstoppableMango/ihfs"),
+		)
+
+		DescribeTable("should parse a release path",
+			func(path string) {
+				fsys := ghfs.New()
+
+				f, err := fsys.Open(path)
+
+				Expect(err).NotTo(HaveOccurred())
+				Expect(f).To(BeAssignableToTypeOf(&ghfs.Release{}))
+				o := f.(*ghfs.Release)
+				Expect(o.Owner()).To(Equal("UnstoppableMango"))
+				Expect(o.Repository()).To(Equal("ihfs"))
+				Expect(o.Name()).To(Equal("v0.1.0"))
+			},
+			Entry(nil, "https://api.github.com/UnstoppableMango/ihfs/releases/tag/v0.1.0"),
+			Entry(nil, "https://github.com/UnstoppableMango/ihfs/releases/tag/v0.1.0"),
+			Entry(nil, "github.com/UnstoppableMango/ihfs/releases/tag/v0.1.0"),
+			Entry(nil, "api.github.com/UnstoppableMango/ihfs/releases/tag/v0.1.0"),
+			Entry(nil, "raw.githubusercontent.com/UnstoppableMango/ihfs/releases/tag/v0.1.0"),
+			Entry(nil, "UnstoppableMango/ihfs/releases/tag/v0.1.0"),
+			Entry(nil, "https://api.github.com/UnstoppableMango/ihfs/releases/download/v0.1.0"),
+			Entry(nil, "https://github.com/UnstoppableMango/ihfs/releases/download/v0.1.0"),
+			Entry(nil, "github.com/UnstoppableMango/ihfs/releases/download/v0.1.0"),
+			Entry(nil, "api.github.com/UnstoppableMango/ihfs/releases/download/v0.1.0"),
+			Entry(nil, "raw.githubusercontent.com/UnstoppableMango/ihfs/releases/download/v0.1.0"),
+			Entry(nil, "UnstoppableMango/ihfs/releases/download/v0.1.0"),
+		)
+
+		DescribeTable("should parse an asset path",
+			func(path string) {
+				fsys := ghfs.New()
+
+				f, err := fsys.Open(path)
+
+				Expect(err).NotTo(HaveOccurred())
+				Expect(f).To(BeAssignableToTypeOf(&ghfs.Asset{}))
+				o := f.(*ghfs.Asset)
+				Expect(o.Owner()).To(Equal("UnstoppableMango"))
+				Expect(o.Repository()).To(Equal("ihfs"))
+				Expect(o.Release()).To(Equal("v0.1.0"))
+				Expect(o.Name()).To(Equal("asset.tar.gz"))
+			},
+			Entry(nil, "https://api.github.com/UnstoppableMango/ihfs/releases/tag/v0.1.0/asset.tar.gz"),
+			Entry(nil, "https://github.com/UnstoppableMango/ihfs/releases/tag/v0.1.0/asset.tar.gz"),
+			Entry(nil, "github.com/UnstoppableMango/ihfs/releases/tag/v0.1.0/asset.tar.gz"),
+			Entry(nil, "api.github.com/UnstoppableMango/ihfs/releases/tag/v0.1.0/asset.tar.gz"),
+			Entry(nil, "raw.githubusercontent.com/UnstoppableMango/ihfs/releases/tag/v0.1.0/asset.tar.gz"),
+			Entry(nil, "UnstoppableMango/ihfs/releases/tag/v0.1.0/asset.tar.gz"),
+			Entry(nil, "https://api.github.com/UnstoppableMango/ihfs/releases/download/v0.1.0/asset.tar.gz"),
+			Entry(nil, "https://github.com/UnstoppableMango/ihfs/releases/download/v0.1.0/asset.tar.gz"),
+			Entry(nil, "github.com/UnstoppableMango/ihfs/releases/download/v0.1.0/asset.tar.gz"),
+			Entry(nil, "api.github.com/UnstoppableMango/ihfs/releases/download/v0.1.0/asset.tar.gz"),
+			Entry(nil, "raw.githubusercontent.com/UnstoppableMango/ihfs/releases/download/v0.1.0/asset.tar.gz"),
+			Entry(nil, "UnstoppableMango/ihfs/releases/download/v0.1.0/asset.tar.gz"),
+		)
+
+		DescribeTable("should parse a branch path",
+			func(path string) {
+				fsys := ghfs.New()
+
+				f, err := fsys.Open(path)
+
+				Expect(err).NotTo(HaveOccurred())
+				Expect(f).To(BeAssignableToTypeOf(&ghfs.Branch{}))
+				o := f.(*ghfs.Branch)
+				Expect(o.Owner()).To(Equal("UnstoppableMango"))
+				Expect(o.Repository()).To(Equal("ihfs"))
+				Expect(o.Name()).To(Equal("main"))
+			},
+			Entry(nil, "https://api.github.com/UnstoppableMango/ihfs/tree/main"),
+			Entry(nil, "https://github.com/UnstoppableMango/ihfs/tree/main"),
+			Entry(nil, "github.com/UnstoppableMango/ihfs/tree/main"),
+			Entry(nil, "api.github.com/UnstoppableMango/ihfs/tree/main"),
+			Entry(nil, "raw.githubusercontent.com/UnstoppableMango/ihfs/tree/main"),
+			Entry(nil, "UnstoppableMango/ihfs/tree/main"),
+		)
+
+		DescribeTable("should parse a content path",
+			func(path string) {
+				fsys := ghfs.New()
+
+				f, err := fsys.Open(path)
+
+				Expect(err).NotTo(HaveOccurred())
+				Expect(f).To(BeAssignableToTypeOf(&ghfs.Content{}))
+				o := f.(*ghfs.Content)
+				Expect(o.Owner()).To(Equal("UnstoppableMango"))
+				Expect(o.Repository()).To(Equal("ihfs"))
+				Expect(o.Branch()).To(Equal("main"))
+				Expect(o.Name()).To(Equal("README.md"))
+			},
+			Entry(nil, "https://api.github.com/UnstoppableMango/ihfs/blob/main/README.md"),
+			Entry(nil, "https://github.com/UnstoppableMango/ihfs/blob/main/README.md"),
+			Entry(nil, "github.com/UnstoppableMango/ihfs/blob/main/README.md"),
+			Entry(nil, "api.github.com/UnstoppableMango/ihfs/blob/main/README.md"),
+			Entry(nil, "raw.githubusercontent.com/UnstoppableMango/ihfs/blob/main/README.md"),
+			Entry(nil, "UnstoppableMango/ihfs/blob/main/README.md"),
+		)
+
+		DescribeTable("should parse a nested content path",
+			func(path string) {
+				fsys := ghfs.New()
+
+				f, err := fsys.Open(path)
+
+				Expect(err).NotTo(HaveOccurred())
+				Expect(f).To(BeAssignableToTypeOf(&ghfs.Content{}))
+				o := f.(*ghfs.Content)
+				Expect(o.Owner()).To(Equal("UnstoppableMango"))
+				Expect(o.Repository()).To(Equal("ihfs"))
+				Expect(o.Branch()).To(Equal("main"))
+				Expect(o.Name()).To(Equal(".github/renovate.json"))
+			},
+			Entry(nil, "https://api.github.com/UnstoppableMango/ihfs/blob/main/.github/renovate.json"),
+			Entry(nil, "https://github.com/UnstoppableMango/ihfs/blob/main/.github/renovate.json"),
+			Entry(nil, "github.com/UnstoppableMango/ihfs/blob/main/.github/renovate.json"),
+			Entry(nil, "api.github.com/UnstoppableMango/ihfs/blob/main/.github/renovate.json"),
+			Entry(nil, "raw.githubusercontent.com/UnstoppableMango/ihfs/blob/main/.github/renovate.json"),
+			Entry(nil, "UnstoppableMango/ihfs/blob/main/.github/renovate.json"),
+		)
+	})
+})
