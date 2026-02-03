46897e78c31d9e74253162d44778917a9af99c06
Erik Rasmussen <erik.rasmussen@unmango.dev>
2026-02-03 12:35:40 -0600

    WIP

--- ghfs/fs.go
+++ ghfs/fs.go
@@ -1,12 +1,26 @@
 package ghfs
 
 import (
+	"context"
+
 	"github.com/google/go-github/v82/github"
 	"github.com/unstoppablemango/ihfs"
 )
 
+type (
+	ContextFunc func(*Fs, ihfs.Operation) context.Context
+)
+
 type Fs struct {
 	client *github.Client
+	ctxFn  ContextFunc
+}
+
+func New() *Fs {
+	return &Fs{
+		client: github.NewClient(nil),
+		ctxFn:  background,
+	}
 }
 
 func (*Fs) Name() string {
@@ -15,4 +29,12 @@ func (*Fs) Name() string {
 
 func (f *Fs) Open(name string) (ihfs.File, error) {
 	return nil, nil
+}
+
+func (f *Fs) context(op ihfs.Operation) context.Context {
+	return f.ctxFn(f, op)
+}
+
+func background(*Fs, ihfs.Operation) context.Context {
+	return context.Background()
 }
