c3b752a981edb00a50effcbda83ed4f184b2260c
UnstoppableMango <erik.rasmussen@unmango.dev>
2026-02-03 22:17:13 -0600

    WIP

--- ghfs/path.go
+++ ghfs/path.go
@@ -25,21 +25,38 @@ type ghpath struct {
 	release    *string
 }
 
-func parse(path string) *ghpath {
+func parse(path string) (*ghpath, error) {
 	var p ghpath
-	parts := strings.SplitN(path, "/", 4)
-	if len(parts) > 0 {
-		p.owner = &parts[0]
-	}
-	if len(parts) > 1 {
-		p.repository = &parts[1]
-	}
-	if len(parts) > 2 {
-		p.branch = &parts[2]
-	}
-	if len(parts) > 3 {
-		p.content = strings.Split(parts[3], "/")
+	var prev string
+
+	for i, seg := range strings.Split(path, "/") {
+		switch i {
+		case 0:
+			p.owner = &seg
+		case 1:
+			p.repository = &seg
+		case 2:
+			switch seg {
+			case "releases", "tree", "refs":
+				prev = seg
+				continue
+			default:
+				return nil, fmt.Errorf("expected one of 'releases', 'tree', or 'refs', got: %s", seg)
+			}
+		case 3:
+			// TODO: non-releases
+			if seg == "tag" || seg == "download" {
+				continue
+			}
+			return nil, fmt.Errorf("expected 'tag' or 'download' segment, got: %s", seg)
+		case 4:
+			// TODO: non-releases
+			p.release = &seg
+		case 5:
+			// TODO: /download links
+			p.asset = &seg
+		}
 	}
 
-	return &p
+	return &p, nil
 }
