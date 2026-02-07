d980fad6c62930e42ad5a32b7ecb138b738faa28
Erik Rasmussen <erik.rasmussen@unmango.dev>
2026-02-07 12:52:16 -0600

    Good AI

--- ghfs/file_test.go
+++ ghfs/file_test.go
@@ -123,7 +123,7 @@ var _ = Describe("File", func() {
 					}),
 				),
 				mock.WithRequestMatchHandler(
-					mock.GetReposReleasesByOwnerByRepoByReleaseId,
+					mock.GetReposReleasesTagsByOwnerByRepoByTag,
 					http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 						w.Write([]byte("invalid json"))
 					}),
