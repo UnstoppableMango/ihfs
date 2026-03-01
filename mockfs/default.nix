10d3d915a90ec203623a804b79921ead9c910b96
UnstoppableMango <erik.rasmussen@unmango.dev>
2026-03-01 17:22:01 -0600

    Separate flake modules

---
+++ mockfs/default.nix
@@ -0,0 +1,15 @@
+{
+  perSystem =
+    { inputs', lib, ... }:
+    let
+      inherit (inputs'.gomod2nix.legacyPackages) buildGoApplication;
+    in
+    {
+      packages.mockfs = buildGoApplication {
+        pname = "mockfs";
+        version = "0.0.1";
+        src = lib.cleanSource ./.;
+        modules = ./gomod2nix.toml;
+      };
+    };
+}
